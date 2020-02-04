65a97e09ee37969144e2c979e7a8a6652bfc072e
kgalindo666 <32023592+kgalindo666@users.noreply.github.com>
2020-02-04 09:22:28 -0800

    add data to mongoDB

--- routes/designers.js
+++ routes/designers.js
@@ -5,8 +5,12 @@ const designersCtrl = require('../controllers/designers');
 router.get('/', designersCtrl.index)
 router.get('/:id', designersCtrl.show)
 router.get('/:id/edit', designersCtrl.edit)
-router.put('/:id', designersCtrl.update)
 router.get('/:id/comments', designersCtrl.newComment)
-router.get('/:id', designersCtrl.myDelete)
+
+router.put('/:id', designersCtrl.update)
+
+router.delete('/:id', designersCtrl.myDelete)
 
 module.exports = router;
+
+
